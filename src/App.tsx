ff7bfa900f4edf6c6c1e40643dea6032f70be95f
KrishSaraf <krish.saraf@gmail.com>
2025-05-16 00:19:30 +0800

    Fix issue with all pages showing on home route by implementing SinglePageApp component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import WorkExperience from './pages/WorkExperience';
@@ -7,30 +7,46 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import AwardsAndPics from './pages/AwardsAndPics';
 
+// Single page application layout component for the home page
+function SinglePageApp() {
+  return (
+    <>
+      <div id="home-section">
+        <Home />
+      </div>
+      <div id="projects-section">
+        <Projects />
+      </div>
+      <div id="awards-section">
+        <AwardsAndPics />
+      </div>
+      <div id="experience-section">
+        <WorkExperience />
+      </div>
+      <div id="skills-section">
+        <Skills />
+      </div>
+      <div id="contact-section">
+        <Contact />
+      </div>
+    </>
+  );
+}
+
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white">
         <Navbar />
         <main>
-          <div id="home-section">
-            <Home />
-          </div>
-          <div id="projects-section">
-            <Projects />
-          </div>
-          <div id="awards-section">
-            <AwardsAndPics />
-          </div>
-          <div id="experience-section">
-            <WorkExperience />
-          </div>
-          <div id="skills-section">
-            <Skills />
-          </div>
-          <div id="contact-section">
-            <Contact />
-          </div>
+          <Routes>
+            <Route path="/" element={<SinglePageApp />} />
+            <Route path="/experience" element={<WorkExperience />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/awards" element={<AwardsAndPics />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </main>
       </div>
     </Router>
